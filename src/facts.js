28f00a237f76a75fc65df2845d00b2577c292f7d
Dayo Adebanjo <aea405@nyu.edu>
2017-06-12 17:16:14 -0400

    external module for facts

---
+++ src/facts.js
@@ -0,0 +1,72 @@
+module.exports = { 
+
+"hp_books" : {
+    "harry potter and the sorcerers stone" : {
+        "description": "In this book, Harry discovers he is a wizard and heads to Hogwarts for the first time",
+        "series_num": "first" 
+    }, 
+    "harry potter and the chamber of secrets" : {
+        "description": "In this book, Harry heads back to Hogwarts for his second year, where mysterious things are stirring",
+        "series_num": "second" 
+    }, 
+    "harry potter and the prizoner of azkaban" : {
+        "description": "In this book, Hogwarts is shaken by the mysterious tales of a Sirius Black, the escaped convict",
+        "series_num": "third" 
+    }, 
+    "harry potter and the goblet of fire" : {
+        "description": "In this book, Harry fights for his life in the Triwizard Tournament, a dangerous competition he has been unwillingly thrown into",
+        "series_num": "fourth" 
+    }, 
+    "harry potter and the order of the phoenix" : {
+        "description": "In this book, Harry butts heads with the new and arguably insane Dolores Umbridge",
+        "series_num": "fifth" 
+    }, 
+    "harry potter and the half blood prince" : {
+        "description": "In this book, Harry finds a mysterious textbook and delves into the ast of his greatest enemy",
+        "series_num": "sixth" 
+    }, 
+    "harry potter and the deathly hallows" : {
+        "description": "This is the last book. I don't want to ruin it for you. You should maybe buy it.",
+        "series_num": "seventh" 
+    }
+}, 
+
+"hp_chars" : {
+    "harry potter" : {
+        "description": " is the main character in the harry potter series",
+    },
+    "ron weasley" : {
+        "description": " is harry potter's best friend in the harry potter series"
+    },
+    "hermione granger" : {
+        "description": " is harry potter's brilliant best friend in the harry potter series"
+    },
+    "hagrid": {
+        "description": " is the gamekeeper at Hogwarts School of Witchcraft and Wizardry"
+    },
+    "draco malfoy" : {
+        "description": " is harry potters slytherin nemesis in the harry potter series"
+    },
+    "sirius black" : {
+        "description": " is harry potter's godfather in the harry potter series"
+    },
+    "remus lupin" : {
+        "description": " is harry's third year defense against the dark arts professor at Hogwarts in the harry potter series"
+    },
+    "james potter" : {
+        "description": " is harry potter's father in the harry potter series"
+    },
+    "lily potter" : {
+        "description": " is harry potter's mother in the harry potter series"
+    },
+    "voldemort" : {
+        "description": " is the main antagonist in the harry potter series"
+    },
+    "the dursleys" : {
+        "description": " are harry's cruel family in the harry potter series"
+    },
+    "albus dumbledore" : {
+        "description": " is harry potter's wise headmaster and mentor in the harry potter series"
+    }
+    }
+}
